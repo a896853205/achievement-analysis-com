d4db9e6ca56bb5404be60712c0690e9525fa6e53
Keanuo <juvenilekeanuo@live.com>
2019-09-13 12:11:39 +0800

    密码修改页面

--- src/page/home/personal/password-controller.js
+++ src/page/home/personal/password-controller.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React from "react";
+
+import { Form, Input, Tooltip, Icon, Button } from "antd";
 class PasswordController extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      formLayout: "horizontal"
+    };
+  }
+
+  handleFormLayoutChange = e => {
+    this.setState({ formLayout: e.target.value });
+  };
+
   render() {
-    return (<div>修改密码</div>);
+    const { formLayout } = this.state;
+    const formItemLayout =
+      formLayout === "horizontal"
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 }
+          }
+        : null;
+    const buttonItemLayout =
+      formLayout === "horizontal"
+        ? {
+            wrapperCol: { span: 14, offset: 4 }
+          }
+        : null;
+    return (
+      <div>
+        <Form layout="horizontal">
+          <Form.Item label="原密码" {...formItemLayout}>
+            <Input placeholder="请输入原密码" />
+          </Form.Item>
+          <Form.Item label="新密码" {...formItemLayout}>
+            <Input placeholder="请输入新密码" />
+          </Form.Item>
+          <Form.Item label="确认密码" {...formItemLayout}>
+            <Input placeholder="请重新输入新密码" />
+          </Form.Item>
+          <Form.Item {...buttonItemLayout}>
+            <Button type="primary">Submit</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
   }
 }
 export default PasswordController;
