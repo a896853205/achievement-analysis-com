41a9abbb7cf73e76dc369caca5647b74eb1c8152
Qccccc's laptop <896853205@qq.com>
2019-09-15 11:19:33 +0800

    完成了学校和专业的展示

---
+++ src/page/home/voluntary/step3/sub-table-controller.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+// 请求文件
+import { launchRequest } from '../../../../util/request';
+import * as APIS from '../../../../constants/api-constants';
+
+// UI组件
+import { Table, Select } from 'antd';
+
+class SubTableController extends React.Component {
+	state = {
+		majorList: []
+	};
+
+	render() {
+		const columns = [
+			{
+				title: '招生专业',
+				dataIndex: 'major_name',
+				key: 'major_name',
+			},
+			{
+				title: '招生人数',
+				dataIndex: 'enrollment_num',
+				key: 'enrollment_num'
+			},
+			{
+				title: '学制/学费',
+				dataIndex: 'major_code',
+				key: 'major_code',
+				render:(text, record) => (<span>{record.school_system}/{record.tuition}</span>)
+			},
+			{
+				title: '投档概率',
+				dataIndex: '',
+				key: '',
+				render:(record) => (<span>-</span>)
+			},
+			{
+				title: '填报',
+				dataIndex: 'option',
+				key: 'option',
+				render:(record) => (
+					<Select placeholder='请选择' style={{ width: 125 }}>
+						<Select.Option value={0}>A专业</Select.Option>
+						<Select.Option value={1}>B专业</Select.Option>
+						<Select.Option value={2}>C专业</Select.Option>
+						<Select.Option value={3}>D专业</Select.Option>
+						<Select.Option value={4}>E专业</Select.Option>
+						<Select.Option value={5}>F专业</Select.Option>
+					</Select>
+				)
+			}
+		];
+
+		return <Table
+			rowKey={(record) => record.major_id}
+			columns={columns}
+			dataSource={this.state.majorList} />;
+	}
+
+	componentDidMount = async () => {
+		let { majorList } = await launchRequest(APIS.GET_MAJOR, {
+			schoolId: this.props.schoolId
+		});
+
+		console.table(majorList);
+
+		this.setState({
+			majorList
+		});
+	};
+}
+export default SubTableController;
