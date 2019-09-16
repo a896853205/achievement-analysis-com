5b1101ad4c341b9281fe8ec4c4fdd3662d7a09b5
Keanuo <juvenilekeanuo@live.com>
2019-09-16 21:38:52 +0800

    更改获取问题接口路由名字

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -32,7 +32,7 @@ export const GetUploadToken = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_UPL
 /**
  * optquestionnaire
  **************************/
-export const GET_QUESTIONNAIRE_TEST = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_QUESTIONNAIRE}/test`;
+export const GET_QUESTIONNAIRE_LIST = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_QUESTIONNAIRE}/getList`;
 export const GET_QUESTIONNAIRE_STATUS = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_QUESTIONNAIRE}/getStatus`;
 export const UPLOAD_QUESTIONNAIRE_RESULT = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_QUESTIONNAIRE}/uploadResult`;
 export const GET_QUESTIONNAIRE_RANK = `${DominConfigs.DOMAIN}${DominConfigs.PART.OPT_QUESTIONNAIRE}/getRank`;
