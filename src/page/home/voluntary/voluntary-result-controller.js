094448117cee2414e4ebef3373cbea65659f38e8
Qccccc's laptop <896853205@qq.com>
2019-09-21 15:57:07 +0800

    结果页加上骨架结构

--- src/page/home/voluntary/voluntary-result-controller.js
+++ src/page/home/voluntary/voluntary-result-controller.js
@@ -3,10 +3,45 @@ import React from 'react';
 // 关于数据模块交互
 import { connect } from 'react-redux';
 
+// UI组件
+import { Divider, Skeleton } from 'antd';
+
+// css
+import '../../../style/voluntary/result.css';
+
 class VoluntaryResultController extends React.Component {
-  render() {
-    return (<div>{this.props.voluntaryResult ? this.props.voluntaryResult.content : '1'}</div>);
-  }
+	render() {
+		return (
+			<div className='voluntary-result-box'>
+				<div className='voluntary-result-content'>
+					<h1>志愿分析评测报告</h1>
+					<Divider />
+					<div>名称: 志愿分析评测报告 位次: 9466 批次: 一批A</div>
+					<div>
+						<h5>志愿选择完备性</h5>
+						<Skeleton />
+					</div>
+					<div>
+						<h5>梯度选择合理性</h5>
+						<Skeleton />
+					</div>
+					<div>
+						<h5>志愿排序合理性</h5>
+						<Skeleton />
+					</div>
+					<div>
+						<h5>志愿数量合理性</h5>
+						<Skeleton />
+					</div>
+					<div>
+						<h5>大计划选择合理性</h5>
+						<Skeleton />
+					</div>
+					{this.props.voluntaryResult ? this.props.voluntaryResult.content : '1'}
+				</div>
+			</div>
+		);
+	}
 }
 // 从store接收state数据
 const mapStateToProps = (store) => {
@@ -20,9 +55,7 @@ const mapStateToProps = (store) => {
 
 // 向store dispatch action
 const mapDispatchToProps = (dispatch) => {
-	return {
-	};
+	return {};
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(VoluntaryResultController);
