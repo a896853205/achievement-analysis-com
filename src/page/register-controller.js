476f229d09741d67e494618fca0ef3349bef98a6
13836005450 <597985642@qq.com>
2019-09-24 01:46:26 +0800

    修改注册页面，增加form

--- src/page/register-controller.js
+++ src/page/register-controller.js
@@ -1,100 +1,140 @@
-import React from 'react';
+import React from "react";
 
 //样式文件
-import '../style/register.css';
+import "../style/register.css";
 
 // UI组件
-import { Card, Input, Button, message } from 'antd';
+import { Card, Input, Button, message, Form } from "antd";
 
 //请求文件
-import { launchRequest } from '../util/request';
-import * as APIS from '../constants/api-constants';
+import { launchRequest } from "../util/request";
+import * as APIS from "../constants/api-constants";
 
 // 密码加密
-import md5 from 'md5';
+import md5 from "md5";
 
 class RegisterController extends React.Component {
-	state = {
-		username: '',
-		password: '',
-		rePassword: '',
-		loading: false
-	};
+  state = {
+    username: "",
+    password: "",
+    rePassword: "",
+    loading: false
+  };
 
-	render() {
-		return (
-			<div className='back'>
-				<Card style={{ width: 300 }} className='test'>
-					<div className='register-title'>智赢</div>
-					<div className='register-sub-title'>祝你智取未来</div>
-					<Input
-						className='register-input'
-						size='large'
-						placeholder='请输入注册账号'
-						onChange={(e) => this.setState({ username: e.target.value })}
-					/>
-					<Input.Password
-						className='register-input'
-						size='large'
-						placeholder='请输入密码'
-						onChange={(e) => this.setState({ password: e.target.value })}
-					/>
-					<Input.Password
-						className='register-input'
-						size='large'
-						placeholder='确认密码'
-						onChange={(e) => this.setState({ rePassword: e.target.value })}
-					/>
-					<Button
-						className='register-button'
-						type='primary'
-						size='large'
-						onClick={this.handleSubmit}
-						loading={this.state.loading}
-					>
-						注册
-					</Button>
-				</Card>
-			</div>
-		);
-	}
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="back">
+        <Card style={{ width: 300 }} className="test">
+          <div className="register-title">智赢</div>
+          <div className="register-sub-title">祝你智取未来</div>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Item className="form-item">
+              {getFieldDecorator("username", {
+                rules: [
+                  {
+                    required: true,
+                    message: "账号不能为空"
+                  }
+                ]
+              })(
+                <Input
+                  className="register-input"
+                  size="large"
+                  placeholder="请输入注册账号"
+                  onChange={e => this.setState({ username: e.target.value })}
+                />
+              )}
+            </Form.Item>
+            <Form.Item className="form-item">
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    required: true,
+                    message: "密码不能为空"
+                  }
+                ]
+              })(
+                <Input.Password
+                  className="register-input"
+                  size="large"
+                  placeholder="请输入密码"
+                  onChange={e => this.setState({ password: e.target.value })}
+                />
+              )}
+            </Form.Item>
+            <Form.Item className="form-item">
+              {getFieldDecorator("rePassword", {
+                rules: [
+                  {
+                    required: true,
+                    message: "确认密码不能为空"
+                  }
+                ]
+              })(
+                <Input.Password
+                  className="register-input"
+                  size="large"
+                  placeholder="确认密码"
+                  onChange={e => this.setState({ rePassword: e.target.value })}
+                />
+              )}
+            </Form.Item>
+            <Form.Item className="form-item">
+              <Button
+                className="register-button"
+                type="primary"
+                size="large"
+                htmlType="submit"
+                loading={this.state.loading}
+              >
+                注册
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    );
+  }
 
-	//注册函数
-	handleSubmit = async () => {
-		let { username, password, rePassword } = this.state;
-		await this.setState({
-			loading: true
-		});
-
-		// 账号密码不能为空
-		if (username === '' || password === '') {
-			message.error('账号或密码不能为空');
-			await this.setState({
-				loading: false
-			});
-			return;
-		}
-
-		if (password !== rePassword) {
-			message.error('两次密码不一致');
-			await this.setState({
-				loading: false
-			});
-			return;
-		}
-
-		// 发送请求
-		await launchRequest(APIS.USER_REGISTER, {
-			username,
-			password: md5(password)
-		});
-
-		await this.setState({
-			loading: false
+  //注册函数
+  handleSubmit = e => {
+    e.preventDefault(); //阻止button submit的默认行为
+    this.setState({
+      loading: true
     });
-    
-    this.props.history.push('/login');
-	};
+    //表单验证
+    this.props.form.validateFields(async (err, values) => {
+      if (!err) {
+        let { username, password, rePassword } = values;
+        if (password !== rePassword) {
+          message.error("两次密码不一致");
+          this.setState({
+            loading: false
+          });
+          return;
+        } else {
+          // 发送请求
+          let result = await launchRequest(APIS.USER_REGISTER, {
+            username,
+            password: md5(password)
+          });
+
+          this.setState({
+            loading: false
+          });
+
+          if (result != null) {
+            this.props.history.push("/login");
+          }
+        }
+      } else {
+        this.setState({
+          loading: false
+        });
+      }
+    });
+  };
 }
 
-export default RegisterController;
+export default Form.create({})(RegisterController);
