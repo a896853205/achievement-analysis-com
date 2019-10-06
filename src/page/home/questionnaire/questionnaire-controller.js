80be342de9a2492432c35b2f2e9c82bac3b97eac
Keanuo <juvenilekeanuo@live.com>
2019-10-06 18:44:06 +0800

    测评系统上一题&重做

--- src/page/home/questionnaire/questionnaire-controller.js
+++ src/page/home/questionnaire/questionnaire-controller.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import { connect } from "react-redux";
-import { actions as questionnaireActions } from "../../../redux/questionnaire-model";
+import { connect } from 'react-redux';
+import { actions as questionnaireActions } from '../../../redux/questionnaire-model';
 
 // 请求文件
-import { launchRequest } from "../../../util/request";
-import * as APIS from "../../../constants/api-constants";
+import { launchRequest } from '../../../util/request';
+import * as APIS from '../../../constants/api-constants';
 
-import "../../../style/questionnaire/questionnaire.css";
+import '../../../style/questionnaire/questionnaire.css';
 
-import { Typography, Button } from "antd";
+import { Typography, Button, Icon, Modal } from 'antd';
+
+const { confirm } = Modal;
 
 const { Title } = Typography;
 class QuestionnaireController extends React.Component {
@@ -18,15 +20,39 @@ class QuestionnaireController extends React.Component {
     ans: [],
     index: 0,
     loading: true,
-    finished: false
+    finished: false,
   };
   render() {
     return (
       <div className="ques-top cont bg-light">
+        <div className="redo">
+          <Button
+            className="redoButton"
+            type="primary"
+            disabled={this.state.index === 0 ? true : false}
+            onClick={() => {
+              this.undo();
+            }}
+          >
+            <Icon type="left" />
+            上一题
+          </Button>
+          <Button
+            className="redoButton"
+            type="primary"
+            disabled={this.state.index === 0 ? true : false}
+            onClick={() => {
+              this.redo();
+            }}
+          >
+            <Icon type="redo" />
+            重做
+          </Button>
+        </div>
         <Typography className="box-min-width ques-title">
           <Title>
             {this.state.loading
-              ? "问卷加载中..."
+              ? '问卷加载中...'
               : `${this.state.questions[this.state.index].id}.${this.state.questions[this.state.index].question}`}
           </Title>
         </Typography>
@@ -63,12 +89,11 @@ class QuestionnaireController extends React.Component {
   componentDidMount = async () => {
     let data = await launchRequest(APIS.GET_QUESTIONNAIRE_LIST, {});
 
-    console.log("data", data);
-
     await this.setState({
       questions: data.questionList,
-      loading: false
+      loading: false,
     });
+
   };
 
   quesDo = async type => {
@@ -76,35 +101,53 @@ class QuestionnaireController extends React.Component {
       return;
     }
     let tempAnswers = this.state.ans;
-    tempAnswers.push(type);
+    tempAnswers[this.state.index] = type;
     await this.setState({
       index:
         this.state.index === this.state.questions.length - 1
           ? this.state.index
           : this.state.index + 1,
-      ans: tempAnswers
+      ans: tempAnswers,
     });
     if (this.state.ans.length === this.state.questions.length) {
-      console.log("答完了");
-      console.log("目前的答案", this.state.ans);
       await launchRequest(APIS.UPLOAD_QUESTIONNAIRE_RESULT, {
-        quesResult: this.state.ans
+        quesResult: this.state.ans,
       });
       await this.setState({
-        finished: true
+        finished: true,
       });
       this.props.setQuesStatus(2);
       return;
     }
   };
+
+  redo = async () => {
+    confirm({
+      title: '确定要重做吗?',
+      okText: '确认',
+      cancelText: '取消',
+      onOk: async () => {
+        await this.setState({
+          index: 0,
+        });
+      },
+      onCancel() {},
+    });
+  };
+
+  undo = async () => {
+    await this.setState({
+      index: this.state.index - 1,
+    });
+  };
 }
 
 const mapStateToProps = store => {
-  const questionnaireStore = store["questionnaireStore"];
+  const questionnaireStore = store['questionnaireStore'];
   let { status } = questionnaireStore;
 
   return {
-    status
+    status,
   };
 };
 
@@ -112,11 +155,11 @@ const mapDispatchToProps = dispatch => {
   return {
     setQuesStatus: pageIndex => {
       dispatch(questionnaireActions.setQuesStatus(pageIndex));
-    }
+    },
   };
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(QuestionnaireController);
