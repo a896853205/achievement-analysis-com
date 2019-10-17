5f9c9f4554742fa51558658d8613928a4585d607
Qccccc's laptop <896853205@qq.com>
2019-10-17 16:26:17 +0800

    完成新闻详细内容展示

--- src/page/news/news-detail-controller.js
+++ src/page/news/news-detail-controller.js
@@ -1,12 +1,61 @@
 import React from 'react';
 
+import moment from 'moment';
+
 // 请求文件
 import { launchRequest } from '@/util/request';
 import * as APIS from '@/constants/api-constants';
 
+// css样式
+import '@/style/news/news-detail.css';
+import { Divider, Icon } from 'antd';
+
 class NewsDetailController extends React.Component {
+  state = {
+    newsTitle: '',
+    createTime: '',
+    from: '',
+    newsContent: '',
+    viewTimes: ''
+  };
   render() {
-    return <div>NewsDetailController</div>;
+    return (
+      <div className='page-inner-width-box news-detail-box'>
+        {/* 文章主体板块 */}
+        <div className='news-detail-left-box'>
+          <div>
+            <h5>{this.state.newsTitle}</h5>
+            <div className='news-describe-box'>
+              <span>{moment(this.state.createTime).format('MM-DD h:mm')}</span>
+              <Divider type="vertical" />
+              <span>来源:{this.state.from}</span>
+              <Divider type="vertical" />
+              <span><Icon type="eye" />{this.state.viewTimes}</span>
+            </div>
+            <div
+              className='news-content-box'
+              dangerouslySetInnerHTML={{
+                __html: this.state.newsContent
+              }}
+            />
+          </div>
+          <div>
+            <p>推荐阅读</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        {/* 热门文章板块 */}
+        <div></div>
+      </div>
+    );
   }
 
   componentDidMount = async () => {
@@ -14,12 +63,20 @@ class NewsDetailController extends React.Component {
     let { uuid, type } = this.props.match.params;
 
     // 调用查询新闻详细信息的接口
-    let data = await launchRequest(APIS.GET_NEWS_DETAIL, {
+    let newsData = await launchRequest(APIS.GET_NEWS_DETAIL, {
       uuid
     });
 
-    console.log(data);
+    this.setState({
+      newsTitle: newsData.title,
+      createTime: newsData.createTime,
+      from: newsData.from,
+      newsContent: newsData.content,
+      viewTimes: newsData.viewTimes
+    });
+
     console.log(type);
   };
 }
+
 export default NewsDetailController;
