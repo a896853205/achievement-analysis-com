d17665deb2dcedd33735c3083aa2052c9abea1b8
Qccccc's laptop <896853205@qq.com>
2019-10-22 16:52:45 +0800

    完成学校详情头部样式

--- src/page/detail/school-detail-controller.jsx
+++ src/page/detail/school-detail-controller.jsx
@@ -1,36 +1,91 @@
 import React from 'react';
 
+// 路由
+import { Link } from 'react-router-dom';
+
+// UI样式
+import '@/style/detail/school-detail.css';
+import { Icon } from 'antd';
+
 export default () => {
   return (
-    <div>
+    <div className='school-detail-box page-inner-width-box'>
+      {/* 学校详情头部数据 */}
       <div>
-        <div>
-          <h2>清华大学</h2>
-          <span>985</span>
-          <span>双一流</span>
-          <span>211</span>
+        <div className='school-detail-title-box'>
+          <h2 className='school-detail-title'>清华大学</h2>
+          <span className='shool-detail-property-tag'>985</span>
+          <span className='shool-detail-property-tag'>双一流</span>
+          <span className='shool-detail-property-tag'>211</span>
         </div>
-        <div>
-          <img src='' alt='' />
-          <div>
+        <div className='school-detail-describe-box'>
+          <img
+            src='https://cdn.dribbble.com/users/1207383/screenshots/6711883/college-night.png'
+            alt=''
+          />
+          <div className='describe-right-box'>
             <ul>
-              <li>1911</li>
-              <li>公立</li>
-              <li>综合</li>
-              <li>教育部</li>
-              <li>本科</li>
-              <li>北京</li>
-              <li>66个</li>
-              <li>58个</li>
+              <li>
+                <Icon
+                  className='describe-icon'
+                  type='clock-circle'
+                  theme='twoTone'
+                  twoToneColor='#ff6666'
+                />
+                <span>1911</span>
+              </li>
+              <li>
+                <Icon className='describe-icon' type='bank' theme='twoTone' />
+                <span>公立</span>
+              </li>
+              <li>
+                <Icon
+                  className='describe-icon'
+                  type='appstore'
+                  theme='twoTone'
+                  twoToneColor='#52c41a'
+                />
+                <span>综合</span>
+              </li>
+              <li>
+                <Icon
+                  className='describe-icon'
+                  type='schedule'
+                  theme='twoTone'
+                  twoToneColor='#ffA02C'
+                />
+                <span>教育部</span>
+              </li>
+              <li>
+                <Icon
+                  className='describe-icon'
+                  type='bulb'
+                  theme='twoTone'
+                  twoToneColor='#9988ff'
+                />
+                <span>本科</span>
+              </li>
+              <li>
+                <Icon
+                  className='describe-icon'
+                  type='environment'
+                  theme='twoTone'
+                  twoToneColor='#ffA02C'
+                />
+                <span>北京</span>
+              </li>
             </ul>
+            <p className='describe-profile-box'>
+              北京大学创办于1898年，初名京师大学堂，是中国第一所国立综合性大学，也是当时中国最高教育行政机关。辛亥革命后，于1912年改为现名。　　
+              作为新文化运动的中心和“五四”运动的策源地，作为中国最早传播马克思主义和民主科学思想的发祥地，作为中国共产党最早的活动基地，北京大学为民族的振兴和解放、...
+              <Link>
+                <span className='describe-profile-more'>全部</span>
+              </Link>
+            </p>
           </div>
         </div>
-        <div>
-          <p>
-            学校简介<span>全部</span>
-          </p>
-        </div>
       </div>
+      {/* 学校详情左边数据 */}
       <div>
         {/* 大学详情左边数据 */}
         {/* 招生简章 */}
@@ -57,6 +112,7 @@ export default () => {
           {/* Table */}
         </div>
       </div>
+      {/* 学校详情右边数据 */}
       <div>
         {/* 大学详情右边数据 */}
         <div>
