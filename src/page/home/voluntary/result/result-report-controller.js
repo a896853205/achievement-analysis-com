3a02294f8ca26e0726a8c6fa58f60d849f71b635
Qccccc's laptop <896853205@qq.com>
2019-11-08 18:59:03 +0800

    修改了志愿分析评测报告页样式

--- src/page/home/voluntary/result/result-report-controller.js
+++ src/page/home/voluntary/result/result-report-controller.js
@@ -4,8 +4,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 // UI组件
-import { Divider, Skeleton, Tag } from 'antd';
+import { Skeleton, Tag } from 'antd';
 import ReactEcharts from 'echarts-for-react';
+import '@/style/voluntary/result.css';
 
 class ResultReportController extends React.Component {
   getOption = result => {
@@ -34,101 +35,112 @@ class ResultReportController extends React.Component {
     return (
       <div className='voluntary-result-content'>
         <h1>志愿分析评测报告</h1>
-        <Divider />
         <div>
           名称: 志愿分析评测报告 批次:
           {this.props.voluntaryResult.lotsName}
         </div>
         <div className='voluntary-result-detail-box'>
-          <div>
-            {this.props.voluntaryResult.completeResult ? (
-              <div>
-                <Divider />
-                <h3 className='voluntary-result-title'>
-                  <span>志愿选择完备性</span>
-                  {this.props.voluntaryResult.completeResult.reasonable ? (
-                    <Tag color='#87d068'>合理</Tag>
-                  ) : (
-                    <Tag color='#f50'>不合理</Tag>
-                  )}
-                </h3>
-                <Divider />
-                <h5 className='result-describe'>
-                  {this.props.voluntaryResult.completeResult.describe}
-                </h5>
-                <p>智赢建议:不要放弃任何一个机会</p>
-                {this.props.voluntaryResult.completeResult.unWriteDetailArr.map(
-                  item => (
-                    <p key={item}>{item}</p>
-                  )
+          {this.props.voluntaryResult.completeResult ? (
+            <div className='voluntary-result-title-box'>
+              <h3
+                className={`voluntary-result-title ${
+                  this.props.voluntaryResult.completeResult.reasonable
+                    ? 'voluntary-result-title-success'
+                    : 'voluntary-result-title-error'
+                }`}
+              >
+                志愿选择完备性
+              </h3>
+              <div className='voluntary-result-describe-box'>
+                {this.props.voluntaryResult.completeResult.reasonable ? (
+                  <Tag color='#87d068'>合理</Tag>
+                ) : (
+                  <Tag color='#f50'>不合理</Tag>
                 )}
-              </div>
-            ) : (
-              <Skeleton />
-            )}
-          </div>
-          <div>
-            <h3 className='voluntary-result-title'>
-              {this.props.voluntaryResult.gradedResult ? (
-                <div>
-                  <Divider />
-                  <h3 className='voluntary-result-title'>
-                    <span>梯度选择合理性</span>
-                    {this.props.voluntaryResult.gradedResult.reasonable ? (
-                      <Tag color='#87d068'>合理</Tag>
-                    ) : (
-                      <Tag color='#f50'>不合理</Tag>
-                    )}
-                  </h3>
-                  <Divider />
-                  <h5 className='result-describe'>
-                    {this.props.voluntaryResult.gradedResult.describe}
-                  </h5>
-                  <p>智赢建议:填报志愿应拉开一定的层次,建议按照院校录取不同集合由高到低顺序选择.高风险、中风险、低风险和最佳匹配各个类别集合志愿不能倒置.</p>
-                  {this.props.voluntaryResult.gradedResult.gradedDetailArr.map(
+                <p>{this.props.voluntaryResult.completeResult.describe}</p>
+                <p>智赢建议:不要放弃任何一个机会</p>
+                <p>
+                  {this.props.voluntaryResult.completeResult.unWriteDetailArr.map(
                     item => (
-                      <p key={item}>{item}</p>
+                      <span key={item}>{item}</span>
                     )
                   )}
-                  <ReactEcharts
-                    option={this.getOption(
-                      this.props.voluntaryResult.gradedResult.schoolScoreArr
-                    )}
-                    style={{ height: '350px' }}
-                    className='questionnaire-chart'
-                  />
-                </div>
-              ) : (
-                <Skeleton />
-              )}
-            </h3>
-          </div>
-          <div>
-            {this.props.voluntaryResult.planResult ? (
-              <div>
-                <Divider />
-                <h3 className='voluntary-result-title'>
-                  <span>大计划选择合理性</span>
-                  {this.props.voluntaryResult.planResult.reasonable ? (
-                    <Tag color='#87d068'>合理</Tag>
-                  ) : (
-                    <Tag color='#f50'>不合理</Tag>
-                  )}
-                </h3>
-                <Divider />
-                <h5 className='result-describe'>
-                  {this.props.voluntaryResult.planResult.describe}
-                </h5>
-                {this.props.voluntaryResult.planResult.planDetailArr.map(
-                  item => (
-                    <p key={item}>{item}</p>
-                  )
-                )}
+                </p>
               </div>
-            ) : (
-              <Skeleton />
-            )}
-          </div>
+            </div>
+          ) : (
+            <Skeleton />
+          )}
+          {this.props.voluntaryResult.gradedResult ? (
+            <div className='voluntary-result-title-box'>
+              <h3
+                className={`voluntary-result-title ${
+                  this.props.voluntaryResult.completeResult.reasonable
+                    ? 'voluntary-result-title-success'
+                    : 'voluntary-result-title-error'
+                }`}
+              >
+                梯度选择合理性
+              </h3>
+              <div className='voluntary-result-describe-box'>
+                {this.props.voluntaryResult.gradedResult.reasonable ? (
+                  <Tag color='#87d068'>合理</Tag>
+                ) : (
+                  <Tag color='#f50'>不合理</Tag>
+                )}
+                <p>{this.props.voluntaryResult.gradedResult.describe}</p>
+                <p>
+                  智赢建议:填报志愿应拉开一定的层次,建议按照院校录取不同集合由高到低顺序选择.高风险、中风险、低风险和最佳匹配各个类别集合志愿不能倒置.
+                </p>
+                <p>
+                  {this.props.voluntaryResult.gradedResult.gradedDetailArr.map(
+                    item => (
+                      <span key={item}>{item}</span>
+                    )
+                  )}
+                </p>
+                <ReactEcharts
+                  option={this.getOption(
+                    this.props.voluntaryResult.gradedResult.schoolScoreArr
+                  )}
+                  style={{ height: '350px' }}
+                  className='questionnaire-chart'
+                />
+              </div>
+            </div>
+          ) : (
+            <Skeleton />
+          )}
+          {this.props.voluntaryResult.planResult ? (
+            <div className='voluntary-result-title-box'>
+              <h3
+                className={`voluntary-result-title ${
+                  this.props.voluntaryResult.completeResult.reasonable
+                    ? 'voluntary-result-title-success'
+                    : 'voluntary-result-title-error'
+                }`}
+              >
+                大计划选择合理性
+              </h3>
+              <div className='voluntary-result-describe-box'>
+                {this.props.voluntaryResult.planResult.reasonable ? (
+                  <Tag color='#87d068'>合理</Tag>
+                ) : (
+                  <Tag color='#f50'>不合理</Tag>
+                )}
+                <p>{this.props.voluntaryResult.planResult.describe}</p>
+                <p>
+                  {this.props.voluntaryResult.planResult.planDetailArr.map(
+                    item => (
+                      <span key={item}>{item}</span>
+                    )
+                  )}
+                </p>
+              </div>
+            </div>
+          ) : (
+            <Skeleton />
+          )}
         </div>
       </div>
     );
