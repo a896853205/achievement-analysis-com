b194a6063c6adcdbfeb9f5b972bddf0595adb41b
任涵彬 <1870012027@qq.com>
2020-11-02 17:10:46 +0800

    feat: 增加page-loading

---
+++ src/page/index-components/page-loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Spin } from 'antd';
+
+import '@/style/page-loading.css';
+
+export default function PageLoading() {
+  return (
+    <div className='page-loading-box'>
+      <Spin size='large' />
+    </div>
+  );
+}
